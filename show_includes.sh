c7d3583230418248736fda2fb62579470994bdae
hevia <fructu@gmail.com>
2011-01-13 15:12:45 +0100

    added show_includes.sh

---
+++ show_includes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+find . -name "*.cpp" > input.txt
+find . -name "*.c" >> input.txt
+find . -name "*.h" >> input.txt
+find . -name "*.cc" >> input.txt
+sed -e 's/^\.\///g' input.txt > input.txt_sed
+mv -f input.txt_sed input.txt
+/opt/hevia_tools/show_includes
+fdp    out.gv -Tpng -o out_fdp.png
+evince out_fdp.png
