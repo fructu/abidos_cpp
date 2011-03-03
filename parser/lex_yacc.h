ae02d9cf9d4a25a24763c705b25b6b62f951ff81
hevia <fructu@gmail.com>
2011-03-03 16:55:11 +0100

    added some funtions members to parser_descent

---
+++ parser/lex_yacc.h
@@ -0,0 +1,43 @@
+/*------------------------------------------------------------------------------
+
+    Proyecto            : ClientSegmentation
+    Codigo              : includes_lex_yacc.h
+    Descripcion         : 
+    Version             : 0.1
+    Autor               : F. Manuel Hevia Garcia
+    Fecha creacion      : 30/12/2010
+    Fecha modificacion  : 
+
+    Observaciones :
+
+            
+------------------------------------------------------------------------------*/
+#ifndef includes_lex_yacc_h
+#define includes_lex_yacc_h
+#ifdef __cplusplus
+extern "C" {
+#include "parser.h"
+#include <stdio.h>
+
+extern int yylineno;
+extern FILE *yyin;
+
+/*
+extern YYSTYPE yylval;
+*/
+extern char lex_file_name[];
+
+int yylex(void);
+
+/*void yyerror (char *s);*/
+/*int  yyparse();*/
+
+extern char *yytext;
+
+int lex_file_init(char *f);
+void lex_file_end(void);
+int yylex_destroy(void);
+}
+#endif
+#endif
+/*----------------------------------------------------------------------------*/
--- parser/parser_descent.h
+++ parser/parser_descent.h
@@ -15,8 +15,19 @@
 #ifndef parser_descent_h
 #define parser_descent_h
 
+#include "lex_yacc.h"
+
 class c_parser_descent
 {
+  private:
+    int yy_actual;
+    
+    int token_get(void);
+    void token_next(void);
+  public:    
+    c_parser_descent();
+    ~c_parser_descent();
+    void yyparse(char * file_name);
 };
 
 #endif
