5a71aead70114ba326f80b30fb26f904734c0f86
hevia <fructu@gmail.com>
2011-03-03 15:10:26 +0100

    added files to do descent parser to will deprecate yacc parser

---
+++ parser/parser_descent.cpp
@@ -0,0 +1,15 @@
+/*------------------------------------------------------------------------------
+
+    Proyecto            : show_includes
+    Codigo              : parser_descent.cpp
+    Descripcion         :
+    Version             : 0.1
+    Autor               : F. Manuel Hevia Garcia
+    Fecha creacion      : 03/03/2011
+    Fecha modificacion  :
+
+    Observaciones :
+
+
+------------------------------------------------------------------------------*/
+#include "parser_descent.cpp"
---
+++ parser/parser_descent.h
@@ -0,0 +1,22 @@
+/*------------------------------------------------------------------------------
+
+    Proyecto            : show_includes
+    Codigo              : parser_descent.h
+    Descripcion         :
+    Version             : 0.1
+    Autor               : F. Manuel Hevia Garcia
+    Fecha creacion      : 03/03/2011
+    Fecha modificacion  :
+
+    Observaciones :
+
+
+------------------------------------------------------------------------------*/
+#ifndef parser_descent_h
+#define parser_descent_h
+
+class c_parser_descent
+{
+};
+
+#endif
