0e7f123938f1eb0e09d1ebc6cdaf02364122bb7b
hevia <fructu@gmail.com>
2011-03-03 15:27:11 +0100

    added main

---
+++ parser/main.cpp
@@ -0,0 +1,23 @@
+/*------------------------------------------------------------------------------
+
+    Proyecto            : 
+    Codigo              : main.cpp
+    Descripcion         : 
+    Version             : 0.1
+    Autor               : F. Manuel Hevia Garcia
+    Fecha creacion      : 03/03/2011
+    Fecha modificacion  : 
+
+    Observaciones:
+        ->
+
+------------------------------------------------------------------------------*/
+#include <stdio.h>
+#include <string.h>
+#include "parser_descent.h"
+
+int main(int argc, char* argv[])
+{
+  printf("abidos v0.0.01\n");
+  return 0;
+}
