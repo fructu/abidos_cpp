d6f901738bf5f0d80f0af1db94fdfa27ceb311ba
ub_x1 <fructu@ubuntu.(none)>
2011-03-06 09:48:15 +0100

    added script to drop printf of debug

---
+++ processor/drop_printf_debug.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#
+# autor:Manuel Hevia
+# description: 
+#
+#-------------------------------------------
+sub p1
+{
+  open(f_in,"< parser_descent_rules.cpp")||die("error open < file");
+  @raw_data=<f_in>;
+  open(f_out,"> parser_descent_rules.cpp_no_debug")||die("error open > file");
+
+  $method_detected = 0;
+  $method_id = "";
+  foreach $l (@raw_data)
+  {
+        chomp($l);
+        $_= $l;
+
+		if( $_ =~ /printf\("##/ )
+		{
+			# i does not save this line
+		}
+		else
+		{
+			print f_out "$_\n";
+		}
+  }
+
+  close(f_in);
+  close(f_out);
+}
+
+p1;
