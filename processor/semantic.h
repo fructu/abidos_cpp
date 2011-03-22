843189a158ce676c70fd69095c8cf7457c5be0b4
hevia <fructu@gmail.com>
2011-03-22 12:47:16 +0100

    symbols have map_base_class to know theirs parents

--- processor/semantic.h
+++ processor/semantic.h
@@ -21,7 +21,8 @@
 class c_semantic
 {
   public:
-    void identifier(c_context context, c_token token);
+    void identifier(c_context & context, c_token token);
+    void class_name(c_context & context, c_token token);
 };
 
 extern c_semantic semantic;
