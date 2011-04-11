8a703968075518510572672f78d9c2b41bfc3b66
hevia <fructu@gmail.com>
2011-04-11 16:40:38 +0200

    parsed free variables

--- processor/semantic.h
+++ processor/semantic.h
@@ -21,22 +21,27 @@
 class c_semantic
 {
 private:
-  t_vector_decl_specifier member_vector_decl_specifier;
+  t_vector_decl_specifier vector_decl_specifier;
 
 public:
-  void push_back_vector_member_decl_specifier(c_decl_specifier decl)
+  void push_back_vector_decl_specifier(c_decl_specifier decl)
   {
-    member_vector_decl_specifier.push_back(decl);
-  } void clear_member_decl_specifier(void)
+    vector_decl_specifier.push_back(decl);
+  }
+
+  void clear_decl_specifier(void)
   {
-    member_vector_decl_specifier.clear();
+    vector_decl_specifier.clear();
   }
 
   void identifier(c_context & context, c_token token);
   void class_specifier_identifier(c_context & context, c_token token);
   void class_member_declarator(c_context & context, c_token token);
   void member_param_declarator(c_context & context, c_token token);
+  void free_declarator(c_context & context, c_token token);
   void member_insert(c_context & context);
+  
+  void declarator_insert(c_context & context);
 
   void class_name(c_context & context, c_token token);
 };
--- processor/test/t8.cpp
+++ processor/test/t8.cpp
@@ -3,4 +3,7 @@
 */
 
 int a;
+int long float b;
+int long float c1,c2;
+short int s1,s2,s3,s4,s5,s6;
 
