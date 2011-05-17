73dc0d7afbdf3eabed7f68d9fcb9099164181bf0
hevia <fructu@gmail.com>
2011-05-17 10:54:07 +0200

    programed trace.cpp

--- processor/trace.cpp
+++ processor/trace.cpp
@@ -12,6 +12,7 @@
 
 
 ------------------------------------------------------------------------------*/
+#include <stdio.h>
 #include "trace.h"
 /*----------------------------------------------------------------------------*/
 int c_trace_node::order_static = 0;
@@ -30,4 +31,68 @@ void c_trace_graph::add(c_trace_node & node, string s)
   node.set(s);
   vector.push_back(node);
 }
+
+c_trace_graph trace;
 /*----------------------------------------------------------------------------*/
+/*
+class c_generator_trace
+{
+private:
+  FILE * f_out;
+  void functions(t_vector_trace_nodes & vector);
+  void calls(t_vector_trace_nodes & vector);
+public:
+  void run(char *p_file_out);
+};
+*/
+void c_generator_trace::functions(t_vector_trace_nodes & vector)
+{
+}
+/*----------------------------------------------------------------------------*/
+void c_generator_trace::calls(t_vector_trace_nodes & vector)
+{
+  unsigned i = 0;
+  unsigned len = vector.size();
+
+  if ( 0 < len )
+    {
+      fprintf(f_out, "  ROOT->");
+      fprintf(f_out, "%s;\n",vector[0].function_name.c_str() );
+    }
+
+  for ( i = 1; i < len; ++i)
+    {
+      fprintf(f_out, "  %s->",vector[0].function_name_previous.c_str() );
+      fprintf(f_out, "%s;",vector[0].function_name.c_str() );
+    }
+}
+/*----------------------------------------------------------------------------*/
+/*
+  echo "digraph G {Hello->World}" | dot -Tpng >hello.png
+*/
+void c_generator_trace::run(char *p_file_out)
+{
+  printf("## void c_generator_trace::run(char * p_file_out [%s])\n",
+         p_file_out);
+
+  f_out = NULL;
+
+  f_out = fopen(p_file_out, "w");
+  if (NULL == f_out)
+    {
+      printf("  c_generator_trace::run() cant fopen()\n");
+      return;
+    }
+
+  fprintf(f_out, "digraph G {");
+  calls(trace.vector);
+  fprintf(f_out, "}");
+
+  if (NULL != f_out)
+    {
+      fclose(f_out);
+      f_out = NULL;
+    }
+}
+/*----------------------------------------------------------------------------*/
+
