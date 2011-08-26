d84a59117502a153d8cc3f2e6d84505b5dbdaee6
hevia <hevia@cronos.(none)>
2011-08-26 13:36:20 +0200

    if directories does not exists will be created processor/check_directories.pl

---
+++ processor/check_directories.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+#
+# autor:Manuel Hevia
+# description:
+#
+#-------------------------------------------
+@directories = ('out', 'test_out');
+
+foreach (@directories) {
+ 	print "  checking existence $_ ...";
+
+    unless(-d $_){
+        print "creating...";
+        mkdir $_ or die "  Couldn't create dir: [$_]\n";
+        print "[ok]\n";        
+    }
+    else
+    {
+        print "[ok]\n";
+    }
+} 
+
+
+
