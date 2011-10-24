899be5bba0b147a6cc6e28335757102c723edd8c
hevia <fructu@gmail.com>
2011-10-24 13:27:45 +0200

    typename is parsing in templates declaration

--- processor/src/parser_descent_rules.cpp
+++ processor/src/parser_descent_rules.cpp
@@ -1526,7 +1526,8 @@ int c_parser_descent::type_parameter(c_trace_node trace_node)
 
     //## todo rest
     token_next(trace_node.get_tab());
-    if ( token_is(CLASS, trace_node) ) {
+    const int vector_id[]={CLASS, TYPENAME, -1};
+    if (token_is_one(  vector_id,trace_node) != 0) {
         c_decl_specifier decl(c_token_get());
         context.template_parameter.vector_decl_specifier.push_back(decl);
 
