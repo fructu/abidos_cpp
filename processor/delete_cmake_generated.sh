991e1ca2538e9f651fa7c092d0d02e7628044900
hevia <fructu@gmail.com>
2011-12-21 11:50:33 +0100

    put all / in the first CMakeLists.txt, fix problem with config.h.in

--- processor/delete_cmake_generated.sh
+++ processor/delete_cmake_generated.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
-rm	CMakeCache.txt
-rm -rf	CMakeFiles/
-rm 	Makefile
-rm 	cmake_install.cmake
-rm 	includes/config.h
-rm 	includes/parser.h
-rm 	"src/\n  /"
-rm 	src/CMakeCache.txt
+rm	-f CMakeCache.txt
+rm -rf CMakeFiles/
+rm 	-f Makefile
+rm 	-f cmake_install.cmake
+rm 	-f includes/config.h
+rm 	-f includes/parser.h
+rm 	-f includes/tokens.h
+#rm 	-f "src/\n  /"
+rm 	-f src/CMakeCache.txt
 rm -rf	src/CMakeFiles/
 #	src/Makefile.bk
-rm -rf	src/cmake_install.cmake
-rm 	src/lexer.c
+rm  -f src/cmake_install.cmake
+rm 	-f src/lexer.c
 #	src/make_out.txt
-rm	src/parser.c
-rm 	src/ts.cpp
-rm 	src/ts.h
-rm 	src/abidos
+rm	-f src/parser.c
+rm 	-f src/ts.cpp
+rm 	-f src/ts.h
+rm 	-f src/abidos
+rm 	-f src/Makefile
+
+rm  -rf includes/CMakeFiles/
+rm  -f	includes/Makefile
+rm 	-r  includes/cmake_install.cmake
+
+
 
