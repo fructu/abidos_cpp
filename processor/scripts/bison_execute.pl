74666a5935bd6f142ba73d3f88df8fe973460241
hevia <fructu@gmail.com>
2012-01-03 16:48:12 +0100

    passing arugments to scripts/bison_execute.pl

--- processor/scripts/bison_execute.pl
+++ processor/scripts/bison_execute.pl
@@ -5,14 +5,36 @@
 #
 #-------------------------------------------
 use strict;
-my $YACC = "yacc";
-my $INCLUDES = "../includes";
 
-my $estado = '';
-$estado=`$YACC -dt parser.y`;
-$estado=`mv -f y.tab.c parser.c`;
-$estado=`echo '#ifndef parser_h' > $INCLUDES/parser.h`;
-$estado=`echo '#define parser_h' >> $INCLUDES/parser.h`;
-$estado=`cat y.tab.h >> $INCLUDES/parser.h`;
-$estado=`echo '#endif' >> $INCLUDES/parser.h`;
-$estado=`rm -f y.tab.h`;
+sub bison_execute
+{
+  my $YACC     = shift;
+  my $SOURCE   = shift;
+  my $INCLUDES = shift;
+#  my $YACC = "yacc";
+#  my $INCLUDES = "../includes";
+
+  my $estado = '';
+  print "$YACC -dt ${SOURCE}parser.y\n";
+  $estado=`$YACC -dt ${SOURCE}parser.y`;
+  $estado=`mv -f ${SOURCE}parser.tab.c ${SOURCE}parser.c`;
+  $estado=`echo '#ifndef parser_h' > ${INCLUDES}parser.h`;
+  $estado=`echo '#define parser_h' >> ${INCLUDES}parser.h`;
+  $estado=`cat ${SOURCE}parser.tab.h >> ${INCLUDES}parser.h`;
+  $estado=`echo '#endif' >> ${INCLUDES}parser.h`;
+  $estado=`rm -f ${SOURCE}parser.tab.h`;
+}
+
+sub main
+{
+  my $num_args = $#ARGV;
+
+  if ($num_args != 2) {
+    print "\n need source path and includes path\n";
+    return;
+  }
+
+  bison_execute($ARGV[0], $ARGV[1], $ARGV[2]);
+}
+
+main();
